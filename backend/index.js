839314561230df42d053c2cdba8f3f74a4827116
nite2048 <nite2048@gmail.com>
2026-05-02 15:05:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import { withRetry } from "./ai/utils.js";
 const app = express();
 
 app.use((req, res, next) => {
-     res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+     res.setHeader("Access-Control-Allow-Origin", "*");
      res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
      res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
      res.setHeader("Access-Control-Allow-Credentials", "true");
